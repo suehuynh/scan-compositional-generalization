bc252792738766ace6b43e024c38a70e6b2f028b
suehuynh <nguyenhuynh01.nh@gmail.com>
2026-04-27 17:10:05 -0400

    feat: update with attn_mask output

--- data/dataset.py
+++ data/dataset.py
@@ -103,23 +103,17 @@ class SCANDataset(Dataset):
         return len(self.examples)
     
     def __getitem__(self, idx):
-        """
-        Get single example.
-        
-        Returns:
-            Tuple of (input_ids, output_ids, input_length, output_length)
-            - input_ids: torch.LongTensor of shape (max_input_length,)
-            - output_ids: torch.LongTensor of shape (max_output_length,)
-            - input_length: int (actual input length before padding)
-            - output_length: int (actual output length before padding)
-        """
         input_tokens, output_tokens = self.examples[idx]
         input_ids = self._tokens_to_ids(input_tokens, self.input_vocab, self.max_input_length)
         output_ids = self._tokens_to_ids(output_tokens, self.output_vocab, self.max_output_length)
-        input_length = len(input_tokens)
-        output_length = len(output_tokens)
         
-        return input_ids, output_ids, input_length, output_length
+        # input_length = len(input_tokens)
+        # output_length = len(output_tokens)
+
+        input_attention_mask = (input_ids != 0).long()
+        output_attention_mask = (output_ids != 0).long()
+        
+        return input_ids, output_ids, input_attention_mask, output_attention_mask
 
 
     def save_vocab(self, filepath):
